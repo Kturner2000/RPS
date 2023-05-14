c51f34b48d4fe8289e66d9df2a6e5a8d79a25b25
Kturner2000 <ksjturner00@gmail.com>
2023-05-14 11:42:38 +1000

    add changes to css I add css changes on js to happen as the end of the game

--- app.js
+++ app.js
@@ -2,41 +2,46 @@ let play = ['rock', 'paper', 'scissors'];
 let wins = 0;
 let losses = 0;
 let draws = 0;
+let gamesPlayed = 0;
+let maxGames = 5;
 // const rock = document.querySelector('.rock');
 // const paper = document.querySelector('.paper');
 // const scissors = document.querySelector('.scissors')
+let playerScore = document.querySelector('.p-count')
+let compScore = document.querySelector('.c-count')
+let drawScore = document.querySelector('.d-count')
+
 
 const options = document.querySelectorAll('.options');
 options.forEach((option) => {
     option.addEventListener("click", function() {
+        if(gamesPlayed < maxGames){
         let userAnswer = this.textContent;
         userAnswer = userAnswer.toLowerCase();
         let compAnswer = play[Math.floor(Math.random() * play.length)];
         compareInputs(userAnswer, compAnswer)
-    })
-})
+    }
+});
+});
 function compareInputs(userAnswer,compAnswer ) {
     const currentMatch = `${userAnswer} vs ${compAnswer}`
     if(userAnswer === compAnswer) {
         draws++
-        alert(`${currentMatch} is a Tie`)
-    }else if ((userAnswer === 'rock' && compAnswer === 'paper') ||
+        gamesPlayed++
+        drawScore.textContent = `Draws: ${draws}`
+    }else if ((userAnswer === 'rock' && compAnswer === 'Paper') ||
     (userAnswer === 'paper' && compAnswer === 'scissors') || 
     (userAnswer === 'scissors' && compAnswer === 'rock')) {
         losses++
-        alert(`${currentMatch} = Computer Wins`)
+        gamesPlayed++
+        compScore.textContent = `Computer: ${losses}`
     } else {
         wins++
-    alert(`${currentMatch} = You Win`)
+        gamesPlayed++
+        playerScore.textContent = `Player: ${wins}`
+
     }
 }
-
-
-
-
-for (let i = 0; i < 5; i++) {
-   
-
      
-}
+
 
